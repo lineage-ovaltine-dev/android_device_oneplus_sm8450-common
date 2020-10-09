31ada338fcdb4823cecb915cf8220da5e3baaa45
Bruno Martins <bgcngm@gmail.com>
2020-10-09 12:19:37 +0100

    sm8450-common: power: Add extension to new aidl power HAL
    Change-Id: I58ac49957d8d28ad904912986d89ac8db4820ff9
    Signed-off-by: pppig236 <priv@pppig236.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -189,7 +189,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
 
 # Recovery
 BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
