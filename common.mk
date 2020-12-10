796ff4c7250474eef76aedeea2de5054f922582d
Rheygine Medel <rmedel@codeaurora.org>
2020-12-09 16:29:57 -0800

    sm8450-common: composer: IDisplayConfig AIDL API implementation
    Change-Id: Iced1c1bbb62cf1922c1a06ee7452ec2af818acb8
    CRs-Fixed: 2871311

--- common.mk
+++ common.mk
@@ -146,6 +146,7 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.config.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
 PRODUCT_COPY_FILES += \
