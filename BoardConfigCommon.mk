1517b706fab69017d5806b76d487da179b629cd8
Roopesh Nataraja <roopeshr@codeaurora.org>
2021-08-04 12:04:05 -0700

    sm8450-common: taro: Remove stale BOARD_BOOTCONFIG 'hardware' parameter
    BOARD_BOOTCONFIG 'hardware' parameter is not needed anymore
    since it's replaced with 'androidboot.hardware'.
    
    Change-Id: I49945e574a1a4be6a8116f2802e57aa6a8be5e5c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,7 +96,6 @@ BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000
 
 BOARD_BOOTCONFIG := \
-    hardware=qcom \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3
