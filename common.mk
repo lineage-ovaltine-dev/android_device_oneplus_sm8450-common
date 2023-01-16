4d00b5d9cc2aafc1c1b44445e155772ab8742dae
Richard Chang <richardycc@google.com>
2023-01-16 09:07:13 +0000

    sm8450-common: mm: support larger zram writeback storage size
    Bug: 264966702
    Test: push to /vendor/etc/
    
    Change-Id: Ic9cc5317fd20ca7bfc83e215a2bd0a3fd155cd9d
    Signed-off-by: pppig236 <priv@pppig236.com>

--- common.mk
+++ common.mk
@@ -172,6 +172,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.vendor_ramdisk \
+    fstab.zram \
     init.class_main.sh \
     init.kernel.post_boot.sh \
     init.oplus.hw.rc \
