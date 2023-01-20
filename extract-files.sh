dfc2fdaa3796415061e31df3f4f3fe10dfcbc763
LuK1337 <priv.luk@gmail.com>
2023-01-20 22:18:13 +0100

    sm8450-common: Register wise light as android.sensor.light
    Sadly a lot of apps aren't aware of config_displayLightSensorType.
    
    Change-Id: Ib1c2f0ba8305dbfe262722f2a1862dafaac973df

--- extract-files.sh
+++ extract-files.sh
@@ -72,6 +72,10 @@ function blob_fixup() {
         vendor/etc/msm_irqbalance.conf)
             sed -i "s/IGNORED_IRQ=27,23,38$/&,115,332/" "${2}"
             ;;
+        vendor/lib64/sensors.ssc.so)
+            sed -i "s/qti.sensor.wise_light/android.sensor.light\x00/" "${2}"
+            "${SIGSCAN}" -p "F1 E9 D3 84 52 49 3F A0 72" -P "F1 A9 00 80 52 09 00 A0 72" -f "${2}"
+            ;;
     esac
 }
 
