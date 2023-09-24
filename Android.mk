fe68e65ae257835b3062b915dab1696ee7702fef
pppig236 <priv@pppig236.com>
2023-09-23 23:58:28 -0400

    sm8450-common: add support for ovaltine
    Change-Id: Ie44b1e32fc0b0ce151a3fabe3d3beb5bb100ac12

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter udon,$(TARGET_DEVICE)),)
+ifneq ($(filter udon ovaltine,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
