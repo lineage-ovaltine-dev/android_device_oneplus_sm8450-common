c5ea79f0c5afce8ae251d6c195cbf412e13fe1be
pppig236 <priv@pppig236.com>
2023-10-12 01:10:17 -0400

    sm8450-common: init: Kang property override from Xiaomi sm6150
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Change-Id: I2b4c0db20c7d8833cf30bc07745624f22066ce32

--- init/init_oplus.cpp
+++ init/init_oplus.cpp
@@ -10,21 +10,41 @@
 #include <sys/_system_properties.h>
 
 using android::base::GetProperty;
+#include "init_oplus.h"
 
 /*
  * SetProperty does not allow updating read only properties and as a result
  * does not work for our use case. Write "OverrideProperty" to do practically
  * the same thing as "SetProperty" without this restriction.
  */
-void OverrideProperty(const char* name, const char* value) {
-    size_t valuelen = strlen(value);
+void OverrideProperty(char const prop[], char const value[]) {
+    prop_info* pi = (prop_info*)__system_property_find(prop);
+    if (pi != nullptr)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
-    prop_info* pi = (prop_info*)__system_property_find(name);
-    if (pi != nullptr) {
-        __system_property_update(pi, value, valuelen);
-    } else {
-        __system_property_add(name, strlen(name), value, valuelen);
+void load_props(int i, int j) {
+    const auto OverrideRoProperty = [](const char* source, const char* prop, const char* value,
+                                       bool product) {
+        std::string PropertyName = "ro.";
+
+        if (product) PropertyName += "product.";
+        if (source != nullptr) PropertyName += source;
+        if (!product) PropertyName += "build.";
+        PropertyName += prop;
+
+        OverrideProperty(PropertyName.c_str(), value);
+    };
+
+    for (const auto& source : RO_PROP_SOURCES) {
+        OverrideRoProperty(source, "device", DEVICES[j], true);
+        OverrideRoProperty(source, "model", MODELS[i], true);
+        OverrideRoProperty(source, "name", MODELS[i], true);
+        OverrideRoProperty(source, "fingerprint", BUILD_FINGERPRINT[i], false);
     }
+    OverrideRoProperty(nullptr, "product", MODELS[i], false);
 }
 
 /*
@@ -33,7 +53,6 @@ void OverrideProperty(const char* name, const char* value) {
  * after the original property has been set.
  */
 void vendor_load_properties() {
-    auto device = GetProperty("ro.product.product.device", "");
     auto prjname = std::stoi(GetProperty("ro.boot.prjname", "0"));
     auto region = std::stoi(GetProperty("ro.boot.hw_region_id", "0"));
 
@@ -44,18 +63,18 @@ void vendor_load_properties() {
             break;
         // ovaltine
         case 21841:  // CN - ACE Pro
-            OverrideProperty("ro.product.product.model", "PGP110");
+            load_props(4, 0);
             break;
         case 21842:  // IN / EEA / NA - 10T
             switch (region) {
                 case 1:  // IN
-                    OverrideProperty("ro.product.product.model", "CPH2413");
+                    load_props(0, 1);
                     break;
                 case 2:  // EEA
-                    OverrideProperty("ro.product.product.model", "CPH2415");
+                    load_props(1, 1);
                     break;
                 case 3:  // NA
-                    OverrideProperty("ro.product.product.model", "CPH2417");
+                    load_props(2, 1);
                     break;
                 default:
                     LOG(ERROR) << "Unexpected project name: " << prjname;
