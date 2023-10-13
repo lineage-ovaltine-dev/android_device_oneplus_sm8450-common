937c55399c45805845a3224731fe2afc46569eaf
pppig236 <priv@pppig236.com>
2023-10-13 09:53:42 -0400

    sm8450-common: init: Bring back dsds detection based on simcardnum.doublesim
     * At least on my NA device, this prop is set to 0
    
      This change reverts commit 535b3f23085207a6bd64423630f8ea6df50e126c
      and commit 9f5a7fbd6fc5f1668c0e304c6d9efce2c4f1caac.
    
    Change-Id: I76128f881b7598547309328c0b9d15a269b40446

--- init/init.oplus.sh
+++ init/init.oplus.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-if grep -qE "androidboot.rf_version=(11|13|21)" /proc/cmdline; then
+if grep -q simcardnum.doublesim=1 /proc/cmdline; then
     setprop vendor.radio.multisim.config dsds
 fi
