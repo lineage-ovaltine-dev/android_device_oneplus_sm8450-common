d01731ba3d3bfee5b2ccb90a3fc28003595d4b3f
LuK1337 <priv.luk@gmail.com>
2023-10-21 16:34:40 +0200

    sm8550-common: Build android.frameworks.sensorservice@1.0.vendor
    Required by camera.qcom.so.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- common.mk
+++ common.mk
@@ -408,6 +408,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.1-service.oplus-multihal \
     libsensorndkbridge \
     sensors.dynamic_sensor_hal \
