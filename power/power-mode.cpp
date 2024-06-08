8438ef7ed5a2248dd7b1dc44d2640b74559a5e0b
DtHnAme <32587943+DtHnAme@users.noreply.github.com>
2024-06-08 22:17:38 +0530

    sm8450-common: power: Implement DT2W via PowerHAL extension
    [inferno0230] Implement it as QTI PowerHAL extension
    
    Co-authored-by: Maitreya25 <maitreyapatni25@gmail.com>
    Signed-off-by: inferno0230 <mail@inferno0230.in>
    Signed-off-by: pppig236 <priv@pppig236.com>

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The LineageOS Project
+ * Copyright (C) 2020-2024 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,17 +15,26 @@
  */
 
 #include <aidl/android/hardware/power/BnPower.h>
+#include <aidl/vendor/oplus/hardware/touch/IOplusTouch.h>
 #include <android-base/file.h>
 #include <android-base/logging.h>
 #include <linux/input.h>
+#include <android/binder_manager.h>
 
-namespace {}  // anonymous namespace
+namespace {
+using ::aidl::vendor::oplus::hardware::touch::IOplusTouch;
+
+std::shared_ptr<IOplusTouch> mTouchService;
+}  // anonymous namespace
 
 namespace aidl {
 namespace android {
 namespace hardware {
 namespace power {
 namespace impl {
+
+#define DOUBLE_TAP_INDEP_ENABLED 1 << 1
+#define DOUBLE_TAP_INDEP_DISABLED 1 << 16
 
 using ::aidl::android::hardware::power::Mode;
 
@@ -39,8 +48,30 @@ bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
     }
 }
 
-bool setDeviceSpecificMode(Mode /*type*/, bool /*enabled*/) {
-    return true;
+bool setDeviceSpecificMode(Mode type, bool enabled) {
+    switch (type) {
+        case Mode::DOUBLE_TAP_TO_WAKE: {
+            const std::string instance = std::string() + IOplusTouch::descriptor + "/default";
+            mTouchService = IOplusTouch::fromBinder(
+                    ndk::SpAIBinder(AServiceManager_waitForService(instance.c_str())));
+            LOG(INFO) << "Power mode: " << toString(type) << " isDoubleTapEnabled: " << enabled;
+
+            // For normal devices
+            int result = 0;
+            mTouchService->touchWriteNodeFile(0, 1, enabled ? "1" : "0", &result);
+            LOG(DEBUG) << "Power-Touch write double_tap_enable result: " << result;
+
+            // For tcm hbp driver devices
+            mTouchService->touchWriteNodeFile(
+                    0, 21,
+                    std::to_string(enabled ? DOUBLE_TAP_INDEP_ENABLED : DOUBLE_TAP_INDEP_DISABLED),
+                    &result);
+            LOG(DEBUG) << "Power-Touch write double_tap_enable_indep result: " << result;
+            return true;
+        }
+        default:
+            return false;
+    }
 }
 
 }  // namespace impl
