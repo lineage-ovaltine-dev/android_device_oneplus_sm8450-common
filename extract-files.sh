bbf751a72b16e1de225e2ea04c07875f8505e026
Michael Bestas <mkbestas@lineageos.org>
2024-07-08 08:56:25 +0300

    sm8550-common: Remove media_codecs_google_c2*
    AOSP codecs can be used by default and it is not necessary to include
    them device side.
    
    Change-Id: I8227b06f0466b614f12fa9008402b92461ea3dd0

--- extract-files.sh
+++ extract-files.sh
@@ -95,7 +95,7 @@ function blob_fixup() {
             ${PATCHELF} --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
             ;;
         vendor/etc/media_codecs_cape.xml|vendor/etc/media_codecs_cape_vendor.xml)
-            sed -Ei "/media_codecs_(google_audio|google_telephony|google_video|vendor_audio)/d" "${2}"
+            sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio)/d" "${2}"
             ;;
         vendor/etc/seccomp_policy/qwesd@2.0.policy)
             echo "pipe2: 1" >> "${2}"
