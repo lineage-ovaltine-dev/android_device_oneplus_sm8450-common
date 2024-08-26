0180eaa48a068ae57557714bc79a15f52afad141
LuK1337 <priv.luk@gmail.com>
2024-08-27 00:00:55 +0200

    sm8450-common: Switch to common device_framework_matrix.xml
    Change-Id: Idc99a59a377bb3284634a62c0a1ae545bcd4e1e2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -93,7 +93,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/device_framework_matrix.xml \
+    hardware/oplus/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
