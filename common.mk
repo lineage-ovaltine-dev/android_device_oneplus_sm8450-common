LineageOS/android_device_oneplus_sm8350-common

sm8350-common: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

common.mk
@@ -334,6 +334,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor \
     libprotobuf-cpp-full \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl
 
 # Sensors

lineage-ovaltine-dev/android_device_oneplus_sm8450-common

Signed-off-by: pppig236 <priv@pppig236.com>

common.mk
@@ -360,7 +360,8 @@ PRODUCT_COPY_FILES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # QMI
 PRODUCT_PACKAGES += \