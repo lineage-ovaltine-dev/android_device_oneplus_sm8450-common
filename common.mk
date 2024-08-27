aa1cabebed220422ce4ae70cbfe7af0551005294
pppig236 <priv@pppig236.com>
2024-08-26 22:19:13 -0400

    sm8450-common: Build GNSS from source
    Change-Id: I52a0071d510ba49f6fd5cc033e3b8d193a7946f9
    Signed-off-by: pppig236 <priv@pppig236.com>
    Signed-off-by: Weikai Kong <priv@pppig236.com>

--- common.mk
+++ common.mk
@@ -139,8 +139,30 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # GPS
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss-aidl-impl-qti \
+    android.hardware.gnss-aidl-service-qti \
+    gnss@2.0-base.policy \
+    gnss@2.0-xtra-daemon.policy \
+    gnss@2.0-xtwifi-client.policy \
+    libbatching \
+    libgeofencing \
+    libgnss \
+    libgnsspps \
+    libgps.utils \
+    libloc_core \
+    liblocation_api
+
+PRODUCT_PACKAGES += \
+    apdr.conf \
+    batching.conf \
+    gnss_antenna_info.conf \
+    gps.conf \
+    izat.conf \
+    lowi.conf \
+    sap.conf \
+    xtwifi.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
