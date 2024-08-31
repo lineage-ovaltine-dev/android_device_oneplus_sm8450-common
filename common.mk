a84e6098a314b9d111606d5d56847c99ac278762
pppig236 <priv@pppig236.com>
2024-08-31 14:54:09 -0400

    sm8450-common: Import init.kernel.post_boot
    Get rid of the QCOM zram setup
    
    Change-Id: I0f0e5ca1f6c7370909c82318842a4d7236ff6ace
    Signed-off-by: pppig236 <priv@pppig236.com>

--- common.mk
+++ common.mk
@@ -173,6 +173,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.vendor_ramdisk \
     init.class_main.sh \
+    init.kernel.post_boot.sh \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
