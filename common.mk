9bb661a231b8b927e8d9bfffa396f9ddcd4657a7
Bruno Martins <bgcngm@gmail.com>
2024-10-04 22:26:51 +0100

    sm8450-common: Switch to AIDL fingerprint HAL[1/2]
    Change-Id: I1e95e82969b20917ab242087a4db43149bf3dd2d
    Signed-off-by: Weikai Kong <priv@pppig236.com>

--- common.mk
+++ common.mk
@@ -131,9 +131,6 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.oplus
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
@@ -257,8 +254,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-# OSENSE
+# Oplus dummy services
 PRODUCT_PACKAGES += \
+    vendor.oplus.hardware.commondcs-service \
     vendor.oplus.hardware.osense.client-service
 
 # Overlays
