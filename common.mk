a9afa816e8b1c318b74a623ea949e3d1eb765d72
LuK1337 <priv.luk@gmail.com>
2025-01-18 18:33:32 +0100

    sm8450-common: Move WfdCommon boot jar declaration to vendor
    Change-Id: I8fa2c0f64692873b432cc252ec88b388a32023a1

--- common.mk
+++ common.mk
@@ -434,9 +434,5 @@ PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
 
-# WiFi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/sm8450-common/sm8450-common-vendor.mk)
