1218545d7dbc2143b9dc0835115bc3175b59bec4
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    sm8450-common: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -128,9 +128,6 @@ TARGET_KERNEL_EXT_MODULES := \
 	qcom/opensource/video-driver \
 	qcom/opensource/wlan/qcacld-3.0/.qca6490
 
-# Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi_charging_enable
-
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := taro
