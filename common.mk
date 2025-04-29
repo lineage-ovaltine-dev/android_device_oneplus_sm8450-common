4b3c07c4b30c11927dc76137ed10b8af6e4a0676
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:21:39 +0100

    sm8450-common: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- common.mk
+++ common.mk
@@ -217,6 +217,8 @@ PRODUCT_PACKAGES += \
     ueventd.oplus.rc \
     ueventd.qcom.rc
 
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_oplus)
+
 # Keymint
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore.xml
