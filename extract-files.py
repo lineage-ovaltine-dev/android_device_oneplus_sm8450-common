Zeus-Junk-Yard/android_device_xiaomi_sm8450-common

sm8450-common: Add libwfdservice_shim_v1 for WFD
e69a5be2449122f90220151f9d01ace15221facf in system/hardware/interfaces
broke the ABI ;'(

Change-Id: I60691f2bab3f52fd9ade36ed6fcf6a2ef13a18c9

extract-files.py
@@ -57,6 +57,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/bin/wfdservice64': blob_fixup()
+        .add_needed('libwfdservice_shim_v1.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()

lineage-ovaltine-dev/android_device_oneplus_sm8450-common

Signed-off-by: Weikai Kong <priv@pppig236.com>

extract-files.py
@@ -96,6 +96,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so')
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so'),
+    'system_ext/bin/wfdservice64': blob_fixup()
+        .add_needed('libwfdservice_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'vendor/etc/libnfc-nci.conf': blob_fixup()