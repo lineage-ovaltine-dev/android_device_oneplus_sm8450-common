Evolution-X-Devices/device_xiaomi_sm8450-common

sm8450-common: Switch to Touch AIDL
Change-Id: I92a19fedb9ea482616c2e3d550e30b69d2a3a5c7

common.mk
@@ -429,7 +429,7 @@ PRODUCT_PACKAGES += \
 
 # Touchscreen
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi
+    vendor.lineage.touch-service.xiaomi
 
 $(call soong_config_set, XIAOMI_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/devices/virtual/touch/touch_dev/bump_sample_rate)
 

lineage-ovaltine-dev/android_device_oneplus_sm8450-common

common.mk
@@ -377,7 +377,7 @@ PRODUCT_PACKAGES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.oplus
+    vendor.lineage.touch-service.oplus
 
 # Update engine
 PRODUCT_PACKAGES += \
@@ -427,8 +427,7 @@ DEVICE_MANIFEST_FILE := \
     $(LOCAL_PATH)/manifest_taro.xml
 
 ODM_MANIFEST_FILES := \
-    $(LOCAL_PATH)/manifest_dsds.xml \
-    $(LOCAL_PATH)/manifest_lineage.xml
+    $(LOCAL_PATH)/manifest_dsds.xml
 
 # WiFi
 PRODUCT_PACKAGES += \