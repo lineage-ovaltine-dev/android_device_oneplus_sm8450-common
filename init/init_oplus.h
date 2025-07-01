636e302db8894709188b548209a87631d0cb9c58
Weikai Kong <priv@pppig236.com>
2025-07-01 09:56:03 -0400

    sm8450-common: Update blobs from CPH2417_11_H.78
    Change-Id: I84ed229dabd8e89d29e4e30c7df717c7acac469e
    Signed-off-by: Weikai Kong <priv@pppig236.com>

--- init/init_oplus.h
+++ init/init_oplus.h
@@ -22,7 +22,7 @@ constexpr const char* DEVICES[] = {
 constexpr const char* BUILD_FINGERPRINT[] = {
         "OnePlus/CPH2413/OP5552L1:13/SKQ1.221119.001/S.123ec2a_6b801_6ff30:user/release-keys",
         "OnePlus/CPH2415/OP5552L1:13/SKQ1.221119.001/S.123ec2a_6b801_6ff30:user/release-keys",
-        "OnePlus/CPH2417/OP5552L1:13/SKQ1.221119.001/S.123ec2a_6b801_6ff30:user/release-keys",
+        "OnePlus/CPH2417/OP5552L1:15/AP3A.240617.008/S.1dff9a8_1cc71-144ca:user/release-keys",
         "OnePlus/CPH2419/OP5552L1:13/SKQ1.221119.001/S.123ec2a_6b801_6ff30:user/release-keys",
         "OnePlus/PGP110/OP5551L1:14/UP1A.230620.001/S.1317e36_a82b-2:user/release-keys",
 };
