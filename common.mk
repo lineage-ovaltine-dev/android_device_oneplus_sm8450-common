e032b90d764440d2229225829dd7118dfb09f152
Weikai Kong <priv@pppig236.com>
2025-08-06 00:42:04 -0400

    Revert "sm8450-common: Switch to OPLUS sensors AIDL"
    This reverts commit 240a1b27752819bceafb6901f3caffcb68cd3949.

--- common.mk
+++ common.mk
@@ -293,7 +293,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.oplus-multihal \
+    android.hardware.sensors-service.multihal \
     sensors.dynamic_sensor_hal \
     sensors.oplus
 
