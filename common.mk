c83394ba5d51497232f82cf4b70e1ecfe0564ea8
pjgowtham <pjgowtham@gmail.com>
2025-08-24 23:04:16 +0530

    sm8450-common: Build dummy oplus performance HAL service
    Camera and prebuilt audio stack requires this to work
    
    Change-Id: I1ba1763c2258e6776a8f796be88ad3b914623c46

--- common.mk
+++ common.mk
@@ -90,6 +90,9 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti.recovery:64
 
 # Camera
+PRODUCT_PACKAGES += \
+    vendor.oplus.hardware.performance-service
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
